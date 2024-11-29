c81fa1ff25276c2fddfc83ff015bdd142445e673
cristian-robert <38420165+cristian-robert@users.noreply.github.com>
2024-11-29 09:42:16 +0200

    Create TestRunResponse.java

---
+++ src/test/java/com/spring/kafkaproj/configuration/TestRunResponse.java
@@ -0,0 +1,12 @@
+
+public class TestRunResponse {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
